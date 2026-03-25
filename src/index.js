2c44936d9f3dadf93ce65daef40fe9642025202c
Pedro_Southier <phsouthier@hotmail.com>
2026-03-24 21:40:59 -0300

    implementado novas ferramentes na aplicacao

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import promptSchemaMain from "./prompts-schema/prompt-schema-main.js";
 
 import createQRCode from "./services/qr-code/create.js";
 import createPassword from "./services/password/create.js";
+import createPriceFormatter from "./services/price-formatter/create.js";
+import createCoupon from "./services/coupon/create.js";
 
 async function main() {
   prompt.get(promptSchemaMain, async (err, choose) => {
@@ -11,6 +13,8 @@ async function main() {
 
     if (choose.select == 1) await createQRCode();
     if (choose.select == 2) await createPassword();
+    if (choose.select == 3) await createPriceFormatter();
+    if (choose.select == 4) await createCoupon();
   });
 
   prompt.start();
--- src/prompts-schema/prompt-schema-main.js
+++ src/prompts-schema/prompt-schema-main.js
@@ -4,10 +4,10 @@ const promptSchemaMain = [
   {
     name: "select",
     description: chalk.yellow.bold(
-      "Escolha a ferramenta (1 - QRCODE) ou (2- PASSWORD)"
+      "Escolha a ferramenta (1 - QRCODE) | (2 - PASSWORD) | (3 - FORMATADOR DE PREÇO) | (4 - CUPOM)"
     ),
-    pattern: /^[1-2]+$/,
-    message: chalk.red.italic("Escolha apenas entre 1 e 2"),
+    pattern: /^[1-4]$/,
+    message: chalk.red.italic("Escolha apenas entre 1 e 4"),
     required: true,
   },
 ];
